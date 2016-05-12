55490c5925b50c7c2debe8255dd15b713cbb1ede
adaqian <ada.qian@qq.com>
2016-05-12 17:55:33 +0800

    websocket demo

--- src/main/java/com/example/myweb/Application.java
+++ src/main/java/com/example/myweb/Application.java
@@ -1,9 +1,13 @@
 package com.example.myweb;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
 
 	}
 
